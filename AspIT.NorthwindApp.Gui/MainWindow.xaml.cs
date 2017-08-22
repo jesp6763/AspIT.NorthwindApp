7faa7ce25a8bde312eb2bd0e6ca74b7751cf02a2
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-22 10:46:55 +0200

    Removed code that is not used

--- AspIT.NorthwindApp.Gui/MainWindow.xaml.cs
+++ AspIT.NorthwindApp.Gui/MainWindow.xaml.cs
@@ -28,8 +28,6 @@ namespace AspIT.NorthwindApp.Gui
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            tilstandCB.SelectedIndex = 0;
-
             /*Test - Start*/
             Employee employee1 = new Employee(@"C:\Users\jesp6763\Pictures\Flaming Skull very small.png") { TitleOfCourtesy = "Hr", FirstName = "Chris", LastName = "McDonald", BirthDate = new DateTime(1956, 11, 13), HireDate = new DateTime(1993, 8, 11) };
             Employee employee2 = new Employee(string.Empty) { TitleOfCourtesy = "Hr", FirstName = "Carl", LastName = "Johnson", BirthDate = new DateTime(1987, 4, 27), HireDate = new DateTime(2003, 5, 17) };
@@ -38,33 +36,9 @@ namespace AspIT.NorthwindApp.Gui
             /*Test - End*/
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            switch(tilstandCB.SelectedIndex)
-            {
-                case 0: // Read-only | Read
-                    deleteEmployeeBtn.Visibility = Visibility.Hidden;
-                    editEmployeeBtn.Visibility = Visibility.Visible;
-                    break;
-                case 1: // Edit | Read, edit, and delete
-                    deleteEmployeeBtn.Visibility = Visibility.Visible;
-                    editEmployeeBtn.Visibility = Visibility.Visible;
-                    break;
-            }
-        }
-
         private void EmployeeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(employeeList.SelectedIndex > -1)
-            {
-                deleteEmployeeBtn.IsEnabled = true;
-                editEmployeeBtn.IsEnabled = true;
-            }
-            else
-            {
-                deleteEmployeeBtn.IsEnabled = false;
-                editEmployeeBtn.IsEnabled = false;
-            }
+
         }
     }
 }
