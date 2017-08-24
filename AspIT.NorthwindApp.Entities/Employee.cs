26fcfaa14cfd6407a45a66a4c7a4c32ce804cd6a
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-24 10:35:15 +0200

    Initialize photo path in constructor

--- AspIT.NorthwindApp.Entities/Employee.cs
+++ AspIT.NorthwindApp.Entities/Employee.cs
@@ -58,7 +58,7 @@ namespace AspIT.NorthwindApp.Entities
         /// <param name="contactInfo">The person's contact informations</param>
         public Employee(string firstName, string lastName, DateTime birthDate, string address, string city, string region, string postalCode, string country, ContactInfo contactInfo) : base(firstName, lastName, birthDate, address, city, region, postalCode, country, contactInfo)
         {
-            
+            PhotoPath = string.Empty;
         }
 
         #region Properties
