d9ffe641be5caa1a697526901cb986efbc4d2914
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-24 14:00:43 +0200

    Added error manager

---
+++ AspIT.NorthwindApp.Gui/ErrorManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.NorthwindApp.Gui
+{
+    public static class ErrorManager
+    {
+        public static List<string> ErrorMessages { get; set; }
+
+
+    }
+}
