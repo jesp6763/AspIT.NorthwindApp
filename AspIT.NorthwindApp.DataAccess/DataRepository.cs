7b65c9c6974f247191e7a1db6f160b396cd62697
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-29 14:11:37 +0200

    Added GetAll method

---
+++ AspIT.NorthwindApp.DataAccess/DataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspIT.NorthwindApp.Entities;
+
+namespace AspIT.NorthwindApp.DataAccess
+{
+    public class DataRepository<TEntity> where TEntity : IPersistable
+    {
+        public List<TEntity> GetAll()
+        {
+            QueryExecutor queryExecuter = new QueryExecutor("Name=Northwind");
+            DataTable data = queryExecuter.Execute($"SELECT * FROM ´dbo.{typeof(TEntity).Name}´");
+            return new List<TEntity>();
+        }
+    }
+}
