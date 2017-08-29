526e2e099c118d6d691ba043de2d7f7243fd0211
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-29 12:05:35 +0200

    Implement from IRepository

--- AspIT.NorthwindApp.DataAccess/Repositories/EmployeeRepository.cs
+++ AspIT.NorthwindApp.DataAccess/Repositories/EmployeeRepository.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AspIT.NorthwindApp.Entities;
 
 namespace AspIT.NorthwindApp.DataAccess.Repositories
 {
-    class EmployeeRepository
+    class EmployeeRepository : IRepository<Employee>
     {
+        public void Add(Employee entity)
+        {
+            
+        }
+
+        public void Delete(Employee entity)
+        {
+            
+        }
+
+        public void Update(Employee entity)
+        {
+            
+        }
+
+        public void Get(int id)
+        {
+            
+        }
     }
 }
