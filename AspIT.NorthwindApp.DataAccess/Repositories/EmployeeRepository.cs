5c20d4fb8960c65d585c1fdd9063b4ac4d84852f
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-29 13:16:05 +0200

    Added summaries to methods, and class

--- AspIT.NorthwindApp.DataAccess/Repositories/EmployeeRepository.cs
+++ AspIT.NorthwindApp.DataAccess/Repositories/EmployeeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,23 +8,43 @@ using AspIT.NorthwindApp.Entities;
 
 namespace AspIT.NorthwindApp.DataAccess.Repositories
 {
-    class EmployeeRepository : IRepository<Employee>
+    /// <summary>
+    /// Represents an employee repository
+    /// </summary>
+    public class EmployeeRepository : IRepository<Employee>
     {
+        /// <summary>
+        /// Adds an employee
+        /// </summary>
+        /// <param name="entity"></param>
         public void Add(Employee entity)
         {
             
         }
 
+
+        /// <summary>
+        /// Delets an employee
+        /// </summary>
+        /// <param name="entity"></param>
         public void Delete(Employee entity)
         {
             
         }
 
+        /// <summary>
+        /// Updates an employee
+        /// </summary>
+        /// <param name="entity"></param>
         public void Update(Employee entity)
         {
             
         }
 
+        /// <summary>
+        /// Gets an employee by id
+        /// </summary>
+        /// <param name="id"></param>
         public void Get(int id)
         {
             
