50d0b2660233359cfbb527c126e8df3d8f2318cc
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-29 13:56:42 +0200

    Added QueryExecutor

---
+++ AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.NorthwindApp.DataAccess
+{
+    public class QueryExecutor
+    {
+        public QueryExecutor(string connectionString)
+        {
+            
+        }
+
+        public DataTable Execute(string sqlQuery)
+        {
+            return null;
+        }
+    }
+}
