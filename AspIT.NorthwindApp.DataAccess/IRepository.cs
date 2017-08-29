c3140f18b43539bded9d8b246a803e81688f72b6
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-29 12:01:55 +0200

    The generic type is set

--- AspIT.NorthwindApp.DataAccess/IRepository.cs
+++ AspIT.NorthwindApp.DataAccess/IRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AspIT.NorthwindApp.Entities;
 
 namespace AspIT.NorthwindApp.DataAccess
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : Entity
     {
         void Add(T entity);
         void Delete(T entity);
