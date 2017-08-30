0a3b1dff4c06ab4ded41a7c14edbe0682d9324f1
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-08-30 13:26:20 +0200

    Placeholder code added

--- AspIT.NorthwindApp.DataAccess/DataRepository.cs
+++ AspIT.NorthwindApp.DataAccess/DataRepository.cs
@@ -19,7 +19,17 @@ namespace AspIT.NorthwindApp.DataAccess
 
         public List<TEntity> GetAll()
         {
-            
+            List<Employee> employeeList = new List<Employee>();
+            QueryExecutor queryExecutor = new QueryExecutor("Name=Northwind");
+            DataSet employees = queryExecutor.Execute($"SELECT * FROM ´{tableName}´");
+
+            foreach(DataRow row in employees.Tables[tableName].Rows)
+            {
+                Employee employee = new Employee(row.Field<string>("TitleOfCourtesy"), row.Field<string>("Title"), row.Field<DateTime>("HireDate"), row.Field<string>("Extension"), row.Field<string>("Notes"), row.Field<int>("ReportsTo"), row.Field<string>("FirstName"), row.Field<string>("LastName"), row.Field<DateTime>("BirthDate"), row.Field<string>("Address"), row.Field<string>("City"), row.Field<string>("Region"), row.Field<string>("PostalCode"), row.Field<string>("Country"), new ContactInfo(row.Field<string>("HomePhone")));
+                employeeList.Add(employee);
+            }
+
+            return employeeList;
         }
 
         public TEntity GetById(int id)
