6a0e7d11a61a650b6ab3f9dd71dab7b47a1d8475
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-31 10:50:03 +0200

    Added content to Execute method

--- AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
+++ AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,15 @@ namespace AspIT.NorthwindApp.DataAccess
 {
     public class QueryExecutor
     {
+        private string connectionString;
+
         /// <summary>
         /// Initializes a new instance of this class
         /// </summary>
         /// <param name="connectionString">The connection string</param>
         public QueryExecutor(string connectionString)
         {
-            
+            this.connectionString = connectionString;
         }
 
         /// <summary>
@@ -25,6 +28,14 @@ namespace AspIT.NorthwindApp.DataAccess
         /// <returns>A dataset as a result from the sql query</returns>
         public DataSet Execute(string sqlQuery)
         {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand(sqlQuery, connection))
+                {
+                    connection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
             return null;
         }
     }
