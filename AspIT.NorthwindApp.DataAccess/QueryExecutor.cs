c09fd3c1132d48c770926aac0334c1615153afb5
Jesper K <jesp6763@edu.campusvejle.dk>
2017-08-31 10:30:07 +0200

    Added summaries

--- AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
+++ AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
@@ -9,11 +9,20 @@ namespace AspIT.NorthwindApp.DataAccess
 {
     public class QueryExecutor
     {
+        /// <summary>
+        /// Initializes a new instance of this class
+        /// </summary>
+        /// <param name="connectionString">The connection string</param>
         public QueryExecutor(string connectionString)
         {
             
         }
 
+        /// <summary>
+        /// Executes a sql query
+        /// </summary>
+        /// <param name="sqlQuery">The sql query to execute</param>
+        /// <returns>A dataset as a result from the sql query</returns>
         public DataSet Execute(string sqlQuery)
         {
             return null;
