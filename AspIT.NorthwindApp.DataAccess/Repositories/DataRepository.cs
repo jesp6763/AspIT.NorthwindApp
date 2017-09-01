e5d3426adfc9c775b6c993f0e11a310e45ee49ae
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-01 11:23:18 +0200

    Forgot to get name

--- AspIT.NorthwindApp.DataAccess/Repositories/DataRepository.cs
+++ AspIT.NorthwindApp.DataAccess/Repositories/DataRepository.cs
@@ -66,7 +66,7 @@ namespace AspIT.NorthwindApp.DataAccess.Repositories
         /// </summary>
         public virtual void Delete(int id)
         {
-            queryExecutor.Execute($"DELETE FROM {tableName} WHERE {typeof(TEntity)}ID={id}");
+            queryExecutor.Execute($"DELETE FROM {tableName} WHERE {typeof(TEntity).Name}ID={id}");
         }
 
         public void SayHi()
