58f572a4e018d437978c972aaaef93011f5329a2
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-01 11:02:00 +0200

    Removed using system.diagnostics

--- AspIT.NorthwindApp.DataAccess/Repositories/EmployeeDataRepository.cs
+++ AspIT.NorthwindApp.DataAccess/Repositories/EmployeeDataRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Diagnostics;
 using AspIT.NorthwindApp.Entities;
 
 namespace AspIT.NorthwindApp.DataAccess.Repositories
