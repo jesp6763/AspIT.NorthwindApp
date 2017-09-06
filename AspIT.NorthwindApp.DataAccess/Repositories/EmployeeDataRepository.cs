888c81c75b29ad34b2ca896d09541980c9f9adb4
Jesper K <jesp6763@edu.campusvejle.dk>
2017-09-06 13:17:29 +0200

    Added summaries

--- AspIT.NorthwindApp.DataAccess/Repositories/EmployeeDataRepository.cs
+++ AspIT.NorthwindApp.DataAccess/Repositories/EmployeeDataRepository.cs
@@ -36,11 +36,20 @@ namespace AspIT.NorthwindApp.DataAccess.Repositories
             return employeeList;
         }
 
+        /// <summary>
+        /// Saves an employee
+        /// </summary>
+        /// <param name="employee">The employee to be saved</param>
         public override void Save(Employee employee)
         {
             queryExecutor.Execute($"INSERT INTO {tableName} (LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region, PostalCode, Country, HomePhone, Extension, Photo, Notes, ReportsTo, PhotoPath) VALUES('{employee.LastName}', '{employee.FirstName}', '{employee.Title}', '{employee.TitleOfCourtesy}', CAST('{employee.BirthDate.Year}-{employee.BirthDate.Month}-{employee.BirthDate.Day}' AS DATETIME), CAST('{employee.HireDate.Year}-{employee.HireDate.Month}-{employee.HireDate.Day}' AS DATETIME), '{employee.Address}', '{employee.City}', '{(employee.Region == string.Empty ? DBNull : employee.Region)}', '{employee.PostalCode}', '{employee.Country}', '{employee.ContactInfo.HomePhone}', '{employee.Extension}', NULL, 'No notes', {employee.ReportsTo?.ToString() ?? DBNull}, '{employee.PhotoPath}')");
         }
 
+        /// <summary>
+        /// Updates an employee
+        /// </summary>
+        /// <param name="id">The id of the employee to update</param>
+        /// <param name="employee">The new employee data</param>
         public override void Update(int id, Employee employee)
         {
             queryExecutor.Execute($"UPDATE {tableName} SET LastName='{employee.LastName}', FirstName='{employee.FirstName}', Title='{employee.Title}', TitleOfCourtesy='{employee.TitleOfCourtesy}', BirthDate=CAST('{employee.BirthDate.Year}-{employee.BirthDate.Month}-{employee.BirthDate.Day}' AS DATETIME), HireDate=CAST('{employee.HireDate.Year}-{employee.HireDate.Month}-{employee.HireDate.Day}' AS DATETIME), Address='{employee.Address}', City='{employee.City}', Region='{(employee.Region == string.Empty ? DBNull : employee.Region)}', PostalCode='{employee.PostalCode}', Country='{employee.Country}', HomePhone='{employee.ContactInfo.HomePhone}', Extension='{employee.Extension}', Notes='{employee.Notes}', ReportsTo={employee.ReportsTo?.ToString() ?? DBNull}, PhotoPath='{employee.PhotoPath}' WHERE EmployeeID={employee.Id}");
