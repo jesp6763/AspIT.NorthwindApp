c0e88366348f39a1fa09c0f28cb2e9795335aff8
Jesper K <jesp6763@edu.campusvejle.dk>
2017-09-06 11:32:10 +0200

    Added 3 GetById tests

--- AspIT.NorthwindApp.DataAccessTests/Repositories/EmployeeDataRepositoryTests.cs
+++ AspIT.NorthwindApp.DataAccessTests/Repositories/EmployeeDataRepositoryTests.cs
@@ -6,28 +6,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AspIT.NorthwindApp.DataAccessTests.Repositories
+namespace AspIT.NorthwindApp.DataAccess.Repositories.Tests
 {
-    [TestClass()]
-    class EmployeeDataRepositoryTests
+    [TestClass]
+    public class EmployeeDataRepositoryTests
     {
-        [TestMethod()]
+        [TestMethod]
         public void SaveTest()
         {
             Assert.Fail();
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see if an employee returns successfully with id
+        /// </summary>
+        [TestMethod]
+        public void GetByIdSuccess()
+        {
+            EmployeeDataRepository repository = new EmployeeDataRepository();
+            repository.GetById(1);
+        }
+
+        /// <summary>
+        /// Tests to see if it gives the correct exception if a invalid id is given
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetByIdFail1()
+        {
+            EmployeeDataRepository repository = new EmployeeDataRepository();
+            repository.GetById(0);
+        }
+
+        /// <summary>
+        /// Tests to see if it gives the correct exception if the given id is less than 0
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GetByIdFail2()
+        {
+            EmployeeDataRepository repository = new EmployeeDataRepository();
+            repository.GetById(-1);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
         {
             Assert.Fail();
         }
     }
 }
-/*
-Reglen er: 1 Test class tester 1 class.
-Så hvis du har en class EmployeeDataRepository der skal Unit Testes, skal du lave en test class der hedder EmployeeDataRepositoryTests.
-Den skal indeholde en hel del test methods, så vi får god "code coverage".
-Der skal testes på om constructoren virker - her skal der laves en test method ConstructorSuccess - og en modsvarende test, hvis der genereres en exception, ConstructorFailsOnException.
-Sørg for at overholde triple A (Arrange, Act, Assert) - og sørg for at 1 test ikke er afhængig af udfaldet af andre tests. 
- */
