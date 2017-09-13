8bfb587c2bc3c438186d718078e7d75e2839bc94
Jesper K <jesp6763@edu.campusvejle.dk>
2017-09-13 14:54:05 +0200

    Added QueryResult class

---
+++ AspIT.NorthwindApp.DataAccess/QueryResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.NorthwindApp.DataAccess
+{
+    public class QueryResult
+    {
+        /// <summary>
+        /// Initializes a new instance of this class
+        /// </summary>
+        /// <param name="dataSet">The dataset of the result</param>
+        /// <param name="rowsAffected">The amount of affected rows</param>
+        public QueryResult(DataSet dataSet, int rowsAffected)
+        {
+            DataSet = dataSet;
+            RowsAffected = rowsAffected;
+        }
+
+        public DataSet DataSet { get; }
+        public int RowsAffected { get; }
+    }
+}
