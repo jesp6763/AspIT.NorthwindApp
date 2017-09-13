32c83fb2a61d7d89466b8ed4816a75dcd8855970
Jesper K <jesp6763@edu.campusvejle.dk>
2017-09-13 14:39:58 +0200

    New solution for executing sql query and getting rows affected

--- AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
+++ AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
@@ -47,17 +47,29 @@ namespace AspIT.NorthwindApp.DataAccess
         /// <returns>A dataset as a result from the sql query, and how many rows were affected</returns>
         /// <exception cref="InvalidOperationException">Thrown when can't open a connection without specifying a data source or server. or the connection is already open.</exception>
         /// <exception cref="SqlException">Thrown when a connection-level error occurred while opening the connection.</exception>
-        public DataSet Execute(string sqlQuery)
+        public (DataSet, int) Execute(string sqlQuery)
         {
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery, connection))
+                /*using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery, connection))
                 {
                     DataSet dataSet = new DataSet();
                     //sqlAdapter.Update(dataSet);
                     sqlAdapter.Fill(dataSet);
                     return dataSet;
+                }*/
+                DataSet dataSet = new DataSet();
+
+                using (SqlCommand sqlCommand = new SqlCommand(sqlQuery, connection))
+                {
+                    int rowsAffected = sqlCommand.ExecuteNonQuery();
+
+                    using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCommand))
+                    {
+                        sqlAdapter.Fill(dataSet);
+                        return (dataSet, rowsAffected);
+                    }
                 }
             }
         }
