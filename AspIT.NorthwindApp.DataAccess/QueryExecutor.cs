19f2a5057f9169ba9dbccd0e52719a2b87ca7ad8
Jesper K <jesp6763@edu.campusvejle.dk>
2017-09-13 14:54:21 +0200

    Changed summary and return type

--- AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
+++ AspIT.NorthwindApp.DataAccess/QueryExecutor.cs
@@ -44,21 +44,14 @@ namespace AspIT.NorthwindApp.DataAccess
         /// Executes a sql query
         /// </summary>
         /// <param name="sqlQuery">The sql query to execute</param>
-        /// <returns>A dataset as a result from the sql query, and how many rows were affected</returns>
+        /// <returns>A query result that contains a dataset and rows affected</returns>
         /// <exception cref="InvalidOperationException">Thrown when can't open a connection without specifying a data source or server. or the connection is already open.</exception>
         /// <exception cref="SqlException">Thrown when a connection-level error occurred while opening the connection.</exception>
-        public (DataSet, int) Execute(string sqlQuery)
+        public QueryResult Execute(string sqlQuery)
         {
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                /*using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery, connection))
-                {
-                    DataSet dataSet = new DataSet();
-                    //sqlAdapter.Update(dataSet);
-                    sqlAdapter.Fill(dataSet);
-                    return dataSet;
-                }*/
                 DataSet dataSet = new DataSet();
 
                 using (SqlCommand sqlCommand = new SqlCommand(sqlQuery, connection))
@@ -68,7 +61,7 @@ namespace AspIT.NorthwindApp.DataAccess
                     using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCommand))
                     {
                         sqlAdapter.Fill(dataSet);
-                        return (dataSet, rowsAffected);
+                        return new QueryResult(dataSet, rowsAffected);
                     }
                 }
             }
